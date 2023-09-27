be87ef3e1358b1a88e71b669d7ef80c21ebf7446
Nom2 Cognom2 <nbuisac@institutmontilivi.cat>
2023-09-27 14:18:54 +0200

    20230927 Fi classe

--- proves/Prova001/Prova001/Program.cs
+++ proves/Prova001/Prova001/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola a tothom!");
+//string? nom;
+//int edat;
+double preu;
+
+Console.Write("Quant val el llibre? ");
+preu = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("El llibre val " + preu);
+
+// Console.Write("Com et dius -> ");
+// nom = Console.ReadLine();
+// Console.WriteLine("Hola " + nom);
+
+//Console.Write("Quants anys tens? ");
+////edat = int.Parse(Console.ReadLine());
+//edat = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Tens " + edat + " anys");
