27b36ea7fb898ee8b25c75638667cb886f5e2e54
Nom2 Cognom2 <nbuisac@institutmontilivi.cat>
2023-10-02 09:30:18 +0200

    20231002 Exercici 0

---
+++ proves/Llista1/Exercici00/Exercici00/Program.cs
@@ -0,0 +1,14 @@
+﻿// Solució d'una equació de primer grau
+// ax + b = 0
+// declarem les variables
+double a, b, x;
+// demanem els valors a i b a l'usuari
+Console.WriteLine("Anem a  resoldre una equació de 1r grau ax + b = 0");
+Console.Write("Entra el valor de a: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Entra el valor de b: "); 
+b = Convert.ToDouble(Console.ReadLine());
+// calculem el valor de x
+x = -b / a;
+// donem la resposta
+Console.WriteLine("La solucio és x = " + x);
