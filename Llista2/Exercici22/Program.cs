4230371b009082775584ae28adf2f4a7aa64f8c6
Nom2 Cognom2 <nbuisac@institutmontilivi.cat>
2023-10-16 10:44:12 +0200

    20231016 Exercicis 22 i 27

---
+++ Llista2/Exercici22/Program.cs
@@ -0,0 +1,9 @@
+﻿// 22.Demanar una lletra i escriure el seu codi ASCII.
+char caracter;
+int numero;
+Console.Write("Entra una lletra -> ");
+caracter = Convert.ToChar(Console.ReadLine());
+Console.WriteLine($"El caracter {caracter} és el codi {(int) caracter}");
+numero = caracter;
+Console.WriteLine($"El caracter {caracter} és el codi {numero}");
+
---
+++ Llista2/Exercici27/Program.cs
@@ -0,0 +1,15 @@
+﻿int any;
+int d1, d2, d3, d4;
+Console.Write("Entra un any -> ");
+any = Convert.ToInt32(Console.ReadLine());
+
+d4 = any % 10;
+any = any / 10;
+d3 = any % 10;
+any = any / 10;
+d2 = any % 10;
+any = any / 10;
+d1 = any % 10;
+any = any / 10;
+
+Console.WriteLine($"L'any dígit a dígit {d1} {d2} {d3} {d4}");
