bedc6a4c84343c059eb7ef40f7bc56e489f62d6c
Nom2 Cognom2 <nbuisac@institutmontilivi.cat>
2023-10-23 09:21:39 +0200

    Llista03 Inicia

---
+++ Llista3/Exercici01/Program.cs
@@ -0,0 +1,8 @@
+﻿// Llista 3 - Exercici 1
+// Equació de primer grau ax + b = b
+// x = -b / a
+// Controlarem que la x no sigui zero, 0
+double a, b, x;
+Console.WriteLine("Anem a resoldre una equació de primer grau de la forma ax + b = 0");
+Console.WriteLine(2 / 0);
+
