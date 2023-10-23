5e6041cc7c43ddba3e23398747331327336f9b06
Narcis Buisac <nbuisac@institutmontilivi.cat>
2023-10-23 09:57:21 +0200

    Llista 3 - Exercici 1 i 3

--- Llista3/Exercici01/Program.cs
+++ Llista3/Exercici01/Program.cs
@@ -4,5 +4,15 @@
 // Controlarem que la x no sigui zero, 0
 double a, b, x;
 Console.WriteLine("Anem a resoldre una equació de primer grau de la forma ax + b = 0");
-Console.WriteLine(2 / 0);
+Console.Write("Entra el valor de a -> ");
+a = Convert.ToDouble(Console.ReadLine());
+if (a == 0) {
+    System.Console.WriteLine("No és equació de primer grau: El valor de a no pot ser 0");
+} else {
+    Console.Write("Entra el valor de b -> ");
+    b = Convert.ToDouble(Console.ReadLine());
+    x = -b / a;
+    Console.WriteLine($"El resultat per la x és {x}");
+}
+Console.WriteLine("Fins la propera");
 
---
+++ Llista3/Exercici03/Program.cs
@@ -0,0 +1,12 @@
+﻿// LLista 3 - Exercici 3
+// Valor absolut d'un número
+double numero, absolut;
+
+Console.Write("Entra un numero per pantalla -> ");
+numero = Convert.ToDouble(Console.ReadLine());
+if (numero >= 0) {
+    absolut = numero;
+} else {
+    absolut = -numero;
+}
+Console.WriteLine($"|{numero}| = {absolut}");
