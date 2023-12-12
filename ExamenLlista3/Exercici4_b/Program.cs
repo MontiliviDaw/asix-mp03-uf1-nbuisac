3dd1c88912385e1727baafd257a1340f3474d6b6
Narcis Buisac <nbuisac@institutmontilivi.cat>
2023-12-12 09:59:54 +0100

    Examen Llista 3 - Grup A

---
+++ ExamenLlista3/Exercici4_b/Program.cs
@@ -0,0 +1,41 @@
+﻿// Examen Llista 3 - Exercici 4
+double preuInicial, kilos, preuFinal, preuFinalAPagar;
+double increment;
+char tipus, categoria;
+
+increment = 0;
+Console.Write("Entra el preu inicial (€) -> ");
+preuInicial = Convert.ToDouble(Console.ReadLine());
+Console.Write("Kilos de raim -> ");
+kilos = Convert.ToDouble(Console.ReadLine());
+Console.Write("Entra el tipus (A/B) -> ");
+tipus = Convert.ToChar(Console.ReadLine());
+if (tipus == 'a' || tipus == 'A') {
+    Console.Write("Entra la categoria (1/2) -> ");
+    categoria = Convert.ToChar(Console.ReadLine());
+    if (categoria == '1') {
+        increment = 0.2;
+    } else if (categoria == '2') {
+        increment = 0.3;
+    }
+} else if (tipus == 'b' || tipus == 'B') {
+    Console.Write("Entra la categoria (1/2) -> ");
+    categoria = Convert.ToChar(Console.ReadLine());
+    if (categoria == '1') {
+        increment = -0.3;
+    } else if (categoria == '2') {
+        increment = -0.5;
+    }
+}
+if (increment != 0) {
+    preuFinal = preuInicial + increment;
+    preuFinalAPagar = preuFinal * kilos;
+    Console.WriteLine($"Preu Incial  : {preuInicial} Euros");
+    Console.WriteLine($"Increment    : {increment} Euros");
+    Console.WriteLine($"Preu Final   : {preuFinal} Euros");
+    Console.WriteLine($"kilos        : {kilos} Euros");
+    Console.WriteLine($"Preu a pagar : {preuFinalAPagar} Euros");
+} else {
+    Console.WriteLine("ERROR");
+}
+
---
+++ ExamenLlista3/Exercici5/Program.cs
@@ -0,0 +1,30 @@
+﻿// Examen Llista 3 - Exercici 5
+double ca, cb, cc, ajuda;
+
+Console.Write("Entra el valor d'un costat -> ");
+ca = Convert.ToDouble(Console.ReadLine());
+Console.Write("Entra el valor d'un altre costat -> ");
+cb = Convert.ToDouble(Console.ReadLine());
+Console.Write("Entra el valor del darrer costat -> ");
+cc = Convert.ToDouble(Console.ReadLine());
+// Mirem si és Pitàgores
+if (cb > ca) {
+    ajuda = ca;
+    ca = cb;
+    cb = ajuda;
+}
+if (cc > ca) {
+    ajuda = ca;
+    ca = cc;
+    cc = ajuda;
+}
+// Ja tenim a ca el valor major
+if (Math.Pow(ca, 2) == Math.Pow(cb, 2) + Math.Pow(cc, 2)) {
+    Console.WriteLine("Compleix Pitàgores");
+} else if (ca == cb && cb == cc) { // Tres costats iguals
+    Console.WriteLine("Equilàter");
+} else if (ca == cb || ca == cc || cb == cc) {
+    Console.WriteLine("Isòsceles");
+} else {
+    Console.WriteLine("Escalè");
+}
