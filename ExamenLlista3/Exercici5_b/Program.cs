27cf51cd1b74d9b0339ae9187640f63719ab408d
Narcis Buisac <nbuisac@institutmontilivi.cat>
2023-12-12 09:59:39 +0100

    Examen Llista 3 - Grup A

---
+++ ExamenLlista3/Exercici5_b/Program.cs
@@ -0,0 +1,29 @@
+﻿// Examen Llista 3 -  Exercici 5
+double ca, cb, cc, ajuda;
+
+Console.Write("Entra el primer costat -> ");
+ca = Convert.ToDouble(Console.ReadLine());
+Console.Write("Entra el segon costat -> ");
+cb = Convert.ToDouble(Console.ReadLine());
+Console.Write("Entra el tercer costat -> ");
+cc = Convert.ToDouble(Console.ReadLine());
+
+// Posarem a ca el valor major de tots
+if (cb > ca && cb >= cc) {
+    ajuda = ca;
+    ca = cb;
+    cb = ajuda;
+} else if (cc > ca && cc >= cb) {
+    ajuda = ca;
+    ca = cc;
+    cc = ajuda;
+}
+if (ca * ca == cb * cb + cc * cc) {
+    Console.WriteLine("Pitàgores");
+} else if (ca == cb && cb == cc) {
+    Console.WriteLine("Equilàter");
+} else if (ca == cb || cb == cc || ca == cc) {
+    Console.WriteLine("Isòceles");
+} else {
+    Console.WriteLine("Escalè");
+}
