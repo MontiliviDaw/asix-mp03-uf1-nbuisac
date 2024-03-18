25389218c3e6ce561f4e749a5f41f4faa2332a64
Narcis Buisac <nbuisac@institutmontilivi.cat>
2024-03-18 11:05:45 +0100

    Llista 7 - Exercici 1 i 3

---
+++ Llista07a/Exercici02/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program {
+    private static void Main(string[] args) {
+        int nota1, nota2;
+        double mitjana;
+
+        nota1 = DemanaNota();
+        nota2 = DemanaNota();
+        mitjana = CalculaMitjana(nota1, nota2);
+        MostraNotaFinal(mitjana);
+    }
+    static void MostraNotaFinal(double notaFinal) {
+        Console.WriteLine($"La nota final és {notaFinal}");
+    }
+
+    static double CalculaMitjana(int n1, int n2) {
+        double m;
+        m = n1 * 0.6 + n2 * 0.4;
+        return m;
+    }
+    static int DemanaNota() {
+        int n;
+
+        Console.Write("Entra un a nota vàlida -> ");
+        n = Convert.ToInt32(Console.ReadLine());
+        while (n < 0 || n > 10) {
+            Console.Write("Nota invàlida: Entra un a nota vàlida -> ");
+            n = Convert.ToInt32(Console.ReadLine());
+        }
+        return n;
+    }
+}
---
+++ Llista07a/Exercici03/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program {
+    private static void Main(string[] args) {
+        int nota1, nota2;
+        double mitjana;
+
+        nota1 = DemanaNota();
+        nota2 = DemanaNota();
+        mitjana = CalculaMitjana(nota1, nota2);
+        MostraNotaFinal(mitjana);
+    }
+    static void MostraNotaFinal(double notaFinal) {
+        Console.WriteLine($"La nota final és {notaFinal}");
+    }
+
+    static double CalculaMitjana(int n1, int n2) {
+        double m;
+        m = n1 * 0.6 + n2 * 0.4;
+        return Math.Round(m, 2);
+    }
+    static int DemanaNota() {
+        int n;
+        bool ok = false;
+
+        Console.Write("Entra un a nota vàlida -> ");
+        n = Convert.ToInt32(Console.ReadLine());
+        while (! EsNotaValida(n) && ! ok ) {
+            Console.Write("Nota invàlida: Entra un a nota vàlida -> ");
+            ok = int.TryParse(Console.ReadLine(), out n);
+        }
+        return n;
+    }
+
+    static bool EsNotaValida(int n) {
+        if (n >= 0 && n <= 10) return true;
+        else return false;
+    }
+}
