944073c9f78193fd70fb079a78423691d28c770f
Narcis Buisac <nbuisac@institutmontilivi.cat>
2024-04-15 10:01:50 +0200

    Serp 1

---
+++ Serp/Serp/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serp {
+    class Class1 {
+        static bool prou = false;
+        static int ample = 1;
+        static int alt = 1;
+        static Semaphore semafor = new Semaphore(1, 1);
+        private static void Main_(string[] args) {
+            Thread t1, t2;
+            ConsoleColor c;
+            ample = Console.WindowWidth;
+            alt = Console.WindowHeight;
+            c = Console.ForegroundColor;
+
+            t1 = new Thread(Escriu1);
+            t2 = new Thread(Escriu2);
+            t2.Start();
+            t1.Start();
+            Thread.Sleep(10000);
+            prou = true;
+            Console.ForegroundColor = c;
+            Console.ReadKey(true);
+        }
+        public static void Escriu1() {
+            string m = "11";
+            int x, y;
+            Random r = new Random();
+            while (!prou) {
+                x = r.Next(ample);
+                y = r.Next(alt);
+                semafor.WaitOne();
+                Console.SetCursorPosition(x, y);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(m);
+                semafor.Release();
+                Thread.Sleep(r.Next(100, 1000));
+            }
+        }
+        public static void Escriu2() {
+            string m = "22";
+            int x, y;
+            Random r = new Random();
+            while (!prou) {
+                x = r.Next(ample);
+                y = r.Next(alt);
+                semafor.WaitOne();
+                Console.SetCursorPosition(x, y);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(m);
+                semafor.Release();
+                Thread.Sleep(r.Next(100, 1000));
+            }
+        }
+    }
+}
---
+++ Serp/Serp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+
+internal class Program {
+    private static void Main(string[] args) {
+        int alt, ample;
+        int posX, posY;
+        ConsoleKeyInfo tecla;
+
+        Inicialitza(out ample, out alt, out posX, out posY);
+        Console.Clear();
+        Console.SetCursorPosition(posX, posY);
+        Console.Write("#");
+
+        tecla = Console.ReadKey();
+        while (tecla.Key != ConsoleKey.Escape) {
+            if (tecla.Key == ConsoleKey.UpArrow) {
+                posY--;
+                if (posY < 0) posY = alt - 1;
+            }
+            else if (tecla.Key == ConsoleKey.DownArrow) {
+                posY = (posY + 1) % alt;
+            }
+            else if (tecla.Key == ConsoleKey.LeftArrow) {
+                posX--;
+                if (posX < 0) posX = ample - 1;
+            }
+            else if (tecla.Key == ConsoleKey.RightArrow) {
+                posX = (posX + 1) % ample;
+            }
+            Console.SetCursorPosition(posX, posY);
+            Console.Write("#");
+            tecla = Console.ReadKey();
+        }
+        Console.ReadKey(true);
+
+
+    }
+    static void Inicialitza(out int ample, out int alt, out int x, out int y) {
+        ample = Console.WindowWidth;
+        alt = Console.WindowHeight;
+        x = ample / 2 - 1;
+        y = alt / 2 - 1;
+
+    }
+}
