1d3cba86055c224c8d552ba355e44ec72f996d8c
Narcis Buisac <nbuisac@institutmontilivi.cat>
2024-04-16 05:49:37 +0200

    Serp 3b

--- Serp/Serp/Program.cs
+++ Serp/Serp/Program.cs
@@ -8,51 +8,63 @@ namespace serp {
         private static void Main(string[] args) {
             int alt, ample;
             int posX, posY;
+            ConsoleColor backAbans, frontAbans;
 
-            
+            backAbans = Console.BackgroundColor;
+            frontAbans = Console.ForegroundColor;
 
             Inicialitza(out ample, out alt, out posX, out posY);
             incX = 0; incY = 0;
+            Console.CursorVisible = false;
             Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
             Console.SetCursorPosition(posX, posY);
             Console.Write("#");
             Thread llegeix = new Thread(LlegirTecla);
             llegeix.Start();
 
             while (! fi) {
+                // Càlcul de la nova posició
+                posX = IncrementaX(posX, incX, ample);
+                IncrementaY(ref posY, incY, alt);
 
-                //                Console.SetCursorPosition(posX, posY);
-                //                Console.Write(" ");
-//                Console.Clear();
-                posX = posX + incX;
-                posY = posY + incY;
-                if (posX < 0) posX = ample - 1;
-                else if (posX >= ample) posX = 0;
-                if (posY < 0) posY = alt- 1;
-                else if (posY >= alt) posY = 0;
                 Console.SetCursorPosition(posX, posY);
-                Console.Write("#");
+                Console.Write("*");
                 Thread.Sleep(100);
             }
             Console.ReadKey(true);
-
-
+            Console.BackgroundColor = backAbans;
+            Console.ForegroundColor = frontAbans;
         }
+
+        public static int IncrementaX(int a, int b, int c) {
+            a = a + b;
+            if (a < 0) a = c - 1;
+            else if (a >= c) a = 0;
+            return a;
+        }
+        public static void IncrementaY(ref int a, int b, int c) {
+            a = a + b;
+            if (a < 0) a = c - 1;
+            else if (a >= c) a = 0;
+        }
+
         static void LlegirTecla() {
             ConsoleKeyInfo tecla;
 
             tecla = Console.ReadKey(true);
             while (tecla.Key != ConsoleKey.Escape) {
-                if (tecla.Key == ConsoleKey.UpArrow) {
+                if (tecla.Key == ConsoleKey.UpArrow || tecla.Key == ConsoleKey.W ) {
                     incX = 0; incY = -1;
                 }
-                else if (tecla.Key == ConsoleKey.DownArrow) {
+                else if (tecla.Key == ConsoleKey.DownArrow || tecla.Key == ConsoleKey.S) {
                     incX = 0; incY = 1;
                 }
-                else if (tecla.Key == ConsoleKey.LeftArrow) {
+                else if (tecla.Key == ConsoleKey.LeftArrow || tecla.Key == ConsoleKey.A) {
                     incX = -1; incY = 0;
                 }
-                else if (tecla.Key == ConsoleKey.RightArrow) {
+                else if (tecla.Key == ConsoleKey.RightArrow || tecla.Key == ConsoleKey.D) {
                     incX = 1; incY = 0;
                 }
                 tecla = Console.ReadKey(true);
@@ -60,7 +72,7 @@ namespace serp {
             fi = true;
             }
         static void Inicialitza(out int ample, out int alt, out int x, out int y) {
-            ample = Console.WindowWidth;
+            ample = Console.WindowWidth / 3 * 2;
             alt = Console.WindowHeight;
             x = ample / 2 - 1;
             y = alt / 2 - 1;
