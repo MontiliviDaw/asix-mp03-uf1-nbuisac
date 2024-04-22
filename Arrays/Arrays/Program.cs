74c679dbc8c7dcfabda61998589b5b9af73b130e
Narcis Buisac <nbuisac@institutmontilivi.cat>
2024-04-22 10:31:53 +0200

    Llista 8 - t

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,49 @@
+﻿// Arrays
+Random r = new Random();
+int[] temperatura;
+int mes;
+
+int[] diesPerMes = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+Console.WriteLine(diesPerMes.Length);
+for (int i = 0; i < diesPerMes.Length; i++) {
+    Console.WriteLine($"El mes { i + 1, 2 } te {diesPerMes[i]} dies");
+}
+
+Console.Write("A quin numero de mes estem -> ");
+mes = Convert.ToInt32(Console.ReadLine());
+
+temperatura = new int[diesPerMes[mes - 1]];
+Console.WriteLine("N'he creat " + temperatura.Length);
+
+// Generem les temperatures
+for(int i = 0; i < temperatura.Length; i++) {
+    temperatura[i] = r.Next(10, 30);
+}
+
+// Mostrem les tempertarures
+for(int i = 0; i < temperatura.Length;i++) {
+    Console.Write($"{temperatura[i], 3}");
+}
+Console.WriteLine();
+
+int maxima, minima;
+
+minima = 30;
+maxima = 0;
+minima = int.MaxValue;
+maxima = int.MinValue;
+minima = maxima = temperatura[0];
+for (int i = 0; i < temperatura.Length ; i++) {
+    if (temperatura[i] < minima) {
+        minima = temperatura[i];
+    }
+    if (temperatura[i] > maxima) {
+        maxima = temperatura[i];
+    }
+}
+Console.WriteLine($"minima = {minima} / maxima = {maxima}");
+
+Console.ReadKey(true);
+
+
---
+++ Llista8/Llista8/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program {
+    private static void Main(string[] args) {
+        int opcio;
+
+        MostraMenu();
+        opcio = DemanaOpcio();
+
+    }
+
+    static int DemanaOpcio() {
+        int o;
+        bool ok;
+        do {
+            ok = int.TryParse(Console.ReadLine(), out o);
+        } while (o < 0 || o > 6 || ok == false);
+
+        
+        return o;
+    }
+
+    static void MostraMenu() {
+        Console.Clear();
+        Console.WriteLine("1.- Omple Dades");
+        Console.WriteLine("2.- Mostra Dades");
+        Console.WriteLine("3.- Omple Dades");
+        Console.WriteLine("4.- Mostra Dades");
+        Console.WriteLine("5.- Omple Dades");
+        Console.WriteLine("6.- Mostra Dades");
+        Console.WriteLine("\n0.- Sortir");
+    }
+}
