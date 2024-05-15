dbfc872fe91b2e7e097d80624dfe1a419129c61b
Narcis Buisac <nbuisac@institutmontilivi.cat>
2024-05-15 14:30:11 +0200

    Exercici 5

---
+++ Fitxers/Exercici005/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program {
+    private static void Main(string[] args) {
+        StreamReader f1, f2;
+        int encerts;
+        string l1, l2;
+        f1 = new StreamReader(@"c:\asix\mp03\f1.txt");
+        f2 = new StreamReader(@"c:\asix\mp03\f2.txt");
+        encerts = 0;
+        while (f1.EndOfStream == false && f2.EndOfStream == false) {
+            l1 = f1.ReadLine();
+            l2 = f2.ReadLine();
+            if (l1 == l2) {
+                encerts++;
+            }
+        }
+        Console.WriteLine($"N'has encertat {encerts}");
+
+    }
+}
